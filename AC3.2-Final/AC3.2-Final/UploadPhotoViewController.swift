855fb9dd503108faf315daacced9de888cb57b38
Victor Zhong <viczhong@gmail.com>
2017-02-15 18:59:22 -0500

    Small change from textfield to textview.

--- AC3.2-Final/AC3.2-Final/UploadPhotoViewController.swift
+++ AC3.2-Final/AC3.2-Final/UploadPhotoViewController.swift
@@ -10,10 +10,10 @@ import UIKit
 import Firebase
 import MobileCoreServices
 
-class UploadPhotoViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class UploadPhotoViewController: UIViewController, UITextViewDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     @IBOutlet weak var picView: UIImageView!
-    @IBOutlet weak var commentField: UITextField!
+    @IBOutlet weak var commentField: UITextView!
     
     @IBOutlet var photoGesture: UITapGestureRecognizer!
     
@@ -59,62 +59,53 @@ class UploadPhotoViewController: UIViewController, UITextFieldDelegate, UIImageP
         picker.dismiss(animated: true)
     }
     
-    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
-        return true
-    }
-    
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         picker.dismiss(animated: true, completion: nil)
     }
     
     func didTapDoneButton() {
         if let comment = commentField.text {
-            if comment != "" {
-                let linkRef = self.databaseReference.childByAutoId()
-                let storage = FIRStorage.storage()
-                let storageRef = storage.reference()
-                let spaceRef = storageRef.child("images/\(linkRef.key)")
-                let user = FIRAuth.auth()?.currentUser
+            let linkRef = self.databaseReference.childByAutoId()
+            let storage = FIRStorage.storage()
+            let storageRef = storage.reference()
+            let spaceRef = storageRef.child("images/\(linkRef.key)")
+            let user = FIRAuth.auth()?.currentUser
+            
+            // downsize the image by compressing it
+            let image = self.picView.image!
+            let jpeg = UIImageJPEGRepresentation(image, 0.5)
+            
+            let metadata = FIRStorageMetadata()
+            metadata.cacheControl = "public,max-age=300";
+            metadata.contentType = "image/jpeg";
+            
+            let _ = spaceRef.put(jpeg!, metadata: metadata) { (metadata, error) in
+                if error != nil {
+                    print("put error")
+                    self.alertUser(text: "Upload Failed!", message: error?.localizedDescription)
+                }
                 
-                // downsize the image by compressing it
-                let image = self.picView.image!
-                let jpeg = UIImageJPEGRepresentation(image, 0.5)
-                
-                let metadata = FIRStorageMetadata()
-                metadata.cacheControl = "public,max-age=300";
-                metadata.contentType = "image/jpeg";
-                
-                let _ = spaceRef.put(jpeg!, metadata: metadata) { (metadata, error) in
-                    if error != nil {
-                        print("put error")
-                        self.alertUser(text: "Upload Failed!", message: error?.localizedDescription)
-                    }
+                if metadata != nil {
+                    // put in the database only if upload succeeded
+                    let pic = Pic(key: linkRef.key, user: (user?.uid)!, comment: comment)
+                    let dict = pic.asDictionary
                     
-                    if metadata != nil {
-                        // put in the database only if upload succeeded
-                        let pic = Pic(key: linkRef.key, user: (user?.uid)!, comment: comment)
-                        let dict = pic.asDictionary
-                        
-                        linkRef.setValue(dict) { (error, reference) in
-                            if let error = error {
-                                print(error)
-                            }
-                            else {
-                                print(reference)
-                                
-                                self.alertUser(text: "Upload Completed!", completion: {
-                                    self.doneButton.isEnabled = false
-                                    self.picView.image = UIImage(named: "camera_icon")
-                                    self.picView.contentMode = .center
-                                    self.commentField.text = ""
-                                } )
-                            }
+                    linkRef.setValue(dict) { (error, reference) in
+                        if let error = error {
+                            print(error)
+                        }
+                        else {
+                            print(reference)
+                            
+                            self.alertUser(text: "Upload Completed!", completion: {
+                                self.doneButton.isEnabled = false
+                                self.picView.image = UIImage(named: "camera_icon")
+                                self.picView.contentMode = .center
+                                self.commentField.text = "Comment here! Or not."
+                            })
                         }
                     }
                 }
-            }
-            else {
-                self.alertUser(text: "Please Enter Some Text")
             }
         }
     }
